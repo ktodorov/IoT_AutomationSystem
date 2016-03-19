6c483c2ede0ddd2454d487929289df7aefe7fff0
ktodorov <kztodorov@outlook.com>
2016-03-19 18:02:44 +0200

    Added Device, Task, Machine server classes

---
+++ AutomationSystemBackground/AutomationManager/Entities/AutomationTask.cs
@@ -0,0 +1,42 @@
+﻿using AutomationManager.Sensors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum Condition
+{
+	LessThan = 0,
+	MoreThan = 1,
+	EqualTo = 2
+};
+
+public enum Sensor
+{
+	Temperature = 0,
+	Light = 1,
+	Pressure = 2,
+	Camera = 3
+};
+
+namespace AutomationManager.Entities
+{
+	public class AutomationTask
+	{
+		public Device IoTDevice;
+
+		public List<UserMachine> Machines;
+
+		public Condition TaskCondition;
+
+		public Sensor DeviceSensor;
+
+		public object Value;
+
+		public AutomationTask()
+		{
+			Machines = new List<UserMachine>();
+		}
+	}
+}
---
+++ AutomationSystemBackground/AutomationManager/Entities/Device.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomationManager.Entities
+{
+	public class Device
+	{
+		public string Id;
+
+		public string Name;
+
+		public List<AutomationTask> Tasks;
+
+		public List<Sensor> Modules;
+
+		public Device()
+		{
+			Tasks = new List<AutomationTask>();
+		}
+	}
+}
