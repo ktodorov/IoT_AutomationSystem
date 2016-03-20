1734bdf219b2137a8b6729f44a589271314dd3b9
ktodorov <kztodorov@outlook.com>
2016-03-20 13:03:57 +0200

    Communication between the IoT device and server

--- AutomationSystemBackground/AutomationManager/Program.cs
+++ AutomationSystemBackground/AutomationManager/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Azure.Devices;
+using Microsoft.ServiceBus.Messaging;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,9 +10,66 @@ namespace AutomationManager
 {
     class Program
     {
-        static void Main(string[] args)
-        {
+		static ServiceClient serviceClient;
+		static string connectionString = "HostName=AutomationSystemHub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=oQnus5yhy5ybc10s8XidG1HOL+SQIRLTzNV7sY2Ba3Q=";
+		static EventProcessorHost eventProcessorHost;
+		public static string LastMessage { get; set; }
 
-        }
-    }
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Send Cloud-to-Device message\n");
+			serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
+
+			//Console.WriteLine("Press any key to send a C2D message.");
+			//Console.ReadLine();
+
+			string eventHubConnectionString = "Endpoint=sb://iothub-ns-automation-24228-c92f97de12.servicebus.windows.net/;SharedAccessKeyName=iothubowner;SharedAccessKey=oQnus5yhy5ybc10s8XidG1HOL+SQIRLTzNV7sY2Ba3Q=";
+			string eventHubName = "automationsystemhub";
+			string storageAccountName = "automationhubstorage";
+			string storageAccountKey = "F+xz9CkJvBVz1o/aYFAuARMe8XSBPVYPf3s70yl4FVGcR4DyP3X1lfgKfoeed19qshiZjPnaBeP0rJPSAp21Gw==";
+			string storageConnectionString = string.Format("DefaultEndpointsProtocol=https;AccountName={0};AccountKey={1}", storageAccountName, storageAccountKey);
+
+			Task.Run(() =>
+			{
+				SendCloudToDeviceMessageAsync().Wait();
+			});
+
+			string eventProcessorHostName = Guid.NewGuid().ToString();
+			eventProcessorHost = new EventProcessorHost(eventProcessorHostName, eventHubName, EventHubConsumerGroup.DefaultGroupName, eventHubConnectionString, storageConnectionString);
+			Console.WriteLine("Registering EventProcessor...");
+			var options = new EventProcessorOptions();
+			options.ExceptionReceived += (sender, e) => { Console.WriteLine(e.Exception); };
+			eventProcessorHost.RegisterEventProcessorAsync<SimpleEventProcessor>(options).Wait();
+
+			Console.WriteLine("Receiving. Press enter key to stop worker.");
+			Console.ReadLine();
+			eventProcessorHost.UnregisterEventProcessorAsync().Wait();
+
+			Console.ReadLine();
+		}
+
+		private static async Task SendCloudToDeviceMessageAsync()
+		{
+			Console.WriteLine("\nReceiving cloud to device messages from service");
+			while (true)
+			{
+				string message = Program.LastMessage;
+				Program.LastMessage = string.Empty;
+
+				if (!string.IsNullOrEmpty(message))
+				{
+					try
+					{
+
+						var commandMessage = new Message(Encoding.ASCII.GetBytes("Cloud to device message." + message));
+						await serviceClient.SendAsync("test", commandMessage);
+					}
+					catch (Exception e)
+					{
+						Console.WriteLine("[Exception] " + e.ToString());
+					}
+				}
+			}
+		}
+	}
 }
